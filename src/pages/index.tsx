b8a68859ef3bcac5113700dc6db7327a48c03d33
MarcosDMT <marcosgav80@gmail.com>
2023-07-12 16:01:36 +0300

    added navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
-import Image from 'next/image'
-
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-   <>
-   Hello
-   </>
-  )
+    <>
+      <section className="bg-[#E5E5E5] h-screen">
+        <div className="px-12 py-4">
+          <Navbar />
+        </div>
+      </section>
+    </>
+  );
 }
