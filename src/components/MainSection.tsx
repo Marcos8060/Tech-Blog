bad2c20e451a902ee3f1e5c12e9f2f144e715b78
MarcosDMT <marcosgav80@gmail.com>
2023-07-12 16:26:46 +0300

    added main section

--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -1,15 +1,22 @@
-import React from 'react'
-import img1 from '../images/h.jpg'
-
+import React from "react";
+import img1 from "../images/h.jpg";
 
 const MainSection = () => {
   return (
     <>
-    <section>
-      <img className='rounded-xl h-[40vh] w-full object-cover' src={img1.src} alt="" />
-    </section>
+      <section className="relative">
+        <img
+          className="rounded-xl h-[40vh] w-full object-cover"
+          src={img1.src}
+          alt=""
+        />
+        <p className="absolute text-white bottom-12 left-5 font-thin">Travel</p>
+        <p className="absolute text-white bottom-5 left-5 font-semibold">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum...
+        </p>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default MainSection
+export default MainSection;
